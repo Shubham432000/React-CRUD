06c23edd01cf9e0004d99ec17be1a2eefe1352e9
Shubham432000 <shubham7410112997@gmail.com>
2023-02-18 16:50:55 +0530

    create-delete

---
+++ src/Component/Todo.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import {CiEdit} from "react-icons/ci"
+import {MdDeleteForever} from "react-icons/md"
+const Todo = () => {
+
+    const [data,setData]=useState("")
+    const [todo,setTodo]=useState([])
+    const [editclick,setEditclick]=useState(false)
+
+const submit=(e)=>{
+    e.preventDefault()
+    setTodo([...todo,data])
+    setData("")
+}
+
+const deleteData=(id)=>{
+let removeData = todo.filter((item,index)=>{
+    return index !== id
+})
+setTodo(removeData)
+}
+
+const editData=(id)=>{
+let data = todo[id]
+console.log(data)
+setEditclick(true)
+}
+  return (
+    <>
+      <div className="flex justify-center">
+        <input
+        onChange={(e)=>setData(e.target.value)}
+          type="search"
+          id="default-search"
+          className=" w-1/4 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search Mockups, Logos..."
+          required
+        />
+        <button
+        onClick={submit}
+          type="submit"
+          className="text-white   bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          {editclick ? "Update" :"Add"}
+        </button>
+      </div>
+
+      <div className="flex justify-center">
+        <table className="w-1/2 text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th className="px-6 py-3">Product name</th>
+              <th className="px-6 py-3">Action</th>
+              <th className="px-6 py-3">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todo.map((item,id)=>{
+
+           return(
+            <tr key={id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                {item}
+              </th>
+              
+              <td className="px-6 py-4"><CiEdit onClick={()=>editData(id)}/></td>
+              <td className="px-6 py-4"><MdDeleteForever onClick={()=>deleteData(id)}/></td>
+            </tr>
+           )
+             })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
