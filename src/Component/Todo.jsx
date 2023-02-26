8e55365ca8c5ba2f3535dd257de3018813eaa96a
Shubham432000 <shubham7410112997@gmail.com>
2023-02-26 22:08:11 +0530

    tailwind

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -45,7 +45,7 @@ const Todo = () => {
         <input
           value={name}
           onChange={(e) => setName(e.target.value)}
-          className=" w-1/4 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className=" w-1/4 p-4 pl-10 text-sm text-gray-00 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Search Mockups, Logos..."
           required
         />
