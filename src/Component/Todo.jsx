486dcf15a3a72b289fa8611e763d6ae2d84c0558
Shubham432000 <shubham7410112997@gmail.com>
2023-03-09 23:14:26 +0530

    modify

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -59,7 +59,7 @@ const Todo = () => {
         </button>:<button
           onClick={updateData}
           type="submit"
-          className="text-white ml-[10px]  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white ml-[10px]  bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Update
         </button>}
