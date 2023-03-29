d1bfab6235896ce903551e07e40cae5e51ef5310
Shubham432000 <shubham7410112997@gmail.com>
2023-03-29 22:59:34 +0530

    modify

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -53,7 +53,7 @@ const Todo = () => {
         {!editclick?<button
           onClick={submit}
           type="submit"
-          className="text-white ml-[13px]   bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white ml-[12px]   bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Add
         </button>:<button
